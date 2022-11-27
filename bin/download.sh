79b301aec346a506ed54bd3a82b07d9a75441bc6
xiyori <foma@shipilov.ru>
2022-11-27 21:33:25 +0300

    echo steps

--- bin/download.sh
+++ bin/download.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #download LjSpeech
+echo "download LjSpeech"
 wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2 -o /dev/null
 mkdir "$1"
 tar -xvf LJSpeech-1.1.tar.bz2 >> /dev/null
@@ -10,6 +11,7 @@ gdown https://drive.google.com/u/0/uc?id=1-EdH0t0loc6vPiuVtXdhsDtzygWNSNZx
 mv train.txt "$1"
 
 #download Waveglow
+echo "download Waveglow"
 gdown https://drive.google.com/u/0/uc?id=1WsibBTsuRg_SF2Z6L6NFRTT-NjEy1oTx
 mkdir -p waveglow/pretrained_model/
 mv waveglow_256channels_ljs_v2.pt ./resources/waveglow_256channels.pt
@@ -20,6 +22,7 @@ echo $(ls mels | wc -l)
 mv mels "$1/mels"
 
 #download alignments
+echo "download alignments"
 wget https://github.com/xcmyz/FastSpeech/raw/master/alignments.zip
 unzip alignments.zip >> /dev/null
 mv alignments "$1/alignments"
