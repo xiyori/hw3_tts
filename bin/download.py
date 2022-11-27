dcf9c4ce41c78502d2053a6ce2f133e89c33bc3c
xiyori <foma@shipilov.ru>
2022-11-27 21:52:48 +0300

    Fix path setting

--- bin/download.py
+++ bin/download.py
@@ -15,12 +15,13 @@ args = parser.parse_args()
 
 config_ = getattr(config, args.config)
 
+env = os.environ.copy()
 if "datasphere" in args.config:
-    sys.path.append("/home/jupyter/.local/bin")
+    env["PATH"] += ":/home/jupyter/.local/bin"
 
 train_config = config_.TrainConfig()
 data_dir = os.path.abspath(train_config.data_dir)
 
 os.chdir("./resources")
 
-subprocess.run(["../bin/download.sh", data_dir])
+subprocess.run(["../bin/download.sh", data_dir], env=env)
