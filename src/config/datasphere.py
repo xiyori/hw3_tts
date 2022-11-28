02b219bd0a3fbae2a24be2517d272d18ee1342c0
xiyori <foma@shipilov.ru>
2022-11-28 13:15:10 +0000

    Save fewer checkpoints

--- src/config/datasphere.py
+++ src/config/datasphere.py
@@ -8,6 +8,6 @@ from .default import MelSpectrogramConfig, FastSpeechConfig
 class TrainConfig(default.TrainConfig):
     device = "cuda:0"
     
-    max_checkpoints = 30
+    max_checkpoints = 20
 
     batch_size = 64
